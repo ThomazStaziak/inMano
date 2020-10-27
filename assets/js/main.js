7d7d4cef2fe5adfa36fecb2f7fdf8f3f9b9e8f50
thomazstaziak <thostaziak@gmail.com>
2020-10-27 15:40:06 -0300

    chore: added the new website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,28 @@
 // handling the menu switch
 const body = document.querySelector("body");
 const menu = document.querySelector("nav#menu");
-const header = document.querySelector('header')
+const header = document.querySelector("header");
 const logo = document.querySelector("img#logo");
-const toggleButton = document.querySelector('#toggleButton')
-const menuItems = document.querySelectorAll('.nav-link')
+const toggleButton = document.querySelector("#toggleButton");
+const menuItems = document.querySelectorAll(".nav-link");
 
-body.onscroll = () => {
-  // switching the menu style if the scroll height is higher than from 0
-  if (window.scrollY == 0 && window.scrollX == 0) {
-      logo.src = './assets/img/logos/logo.png'
-      toggleButton.style.filter = 'none'
-      header.classList.add('bg-white');
-      menu.classList.add('border-dark')
-      menuItems.forEach(menuItem => {
-          menuItem.classList.remove('text-white')
-          menuItem.classList.add('text-dark')
-      })
-} else {
-    logo.src = './assets/img/logos/logo-mono.png'
-    toggleButton.style.filter = 'invert(1)'
-    header.classList.remove('bg-white');
-    header.style.backgroundColor = '#3E4F74'
-    menu.classList.remove('border-dark')
-    menuItems.forEach(menuItem => {
-        menuItem.classList.remove('text-dark')
-        menuItem.classList.add('text-white')
-    })
-  }
-};
+// body.onscroll = () => {
+//   // switching the menu style if the scroll height is higher than from 0
+//   if (window.scrollY == 0 && window.scrollX == 0) {
+//     logo.src = "./assets/img/logos/logo.png";
+//     toggleButton.style.filter = "none";
+//     header.classList.add("bg-grey");
+//     menuItems.forEach((menuItem) => {
+//       menuItem.classList.remove("text-white");
+//       menuItem.classList.add("text-dark");
+//     });
+//   } else {
+//     toggleButton.style.filter = "invert(1)";
+//     menuItems.forEach((menuItem) => {
+//       menuItem.classList.remove("text-dark");
+//       menuItem.classList.add("text-white");
+//     });
+//   }
+// };
 
-AOS.init()
+AOS.init();
